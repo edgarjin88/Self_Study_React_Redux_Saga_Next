557b2da7cd38704e0040f97adac036f876d0a632
text name <edgar.jin88@gmail.com>
2020-05-04 17:42:14 +1000

    updated boiler plate

--- Next.js/next2/server.js
+++ Next.js/next2/server.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const next = require('next');
-const routes = require('./routes');
+const express = require("express");
+const next = require("next");
+const routes = require("./routes");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = routes.getRequestHandler(app);
+//여기서의 앱은 next의 데브모드
+// routes == "next-routes"
+//app 은 next
+//server express.
+// 다시 말해 모든 리퀘스트는 next에서 처리한다.
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-app.prepare()
-.then(() => {
-  const server = express();
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
+    server.use(handle).listen(3000, err => {
+      if (err) throw err;
+      console.log("> Ready on http://localhost:3000");
+    });
   })
-
-  server.use(handle).listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
